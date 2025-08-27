a1a3a7bfead637ecec3c91122a1573085dbc9435
azunisoftware18 <azunisoftware18@gmail.com>
2025-08-27 11:50:56 +0530

    `Update allowed origins in CORS configuration`

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const data = "50mb";
 app.use(
   cors({
     origin: (origin, callback) => {
-      const allowed = ["https://email.primewebdev.in", "http://localhost:5173"];
+      const allowed = ["https://airmailo.primewebdev.in", "http://localhost:5173"];
       if (!origin || allowed.includes(origin)) {
         callback(null, true);
       } else {
