1b205ca1c2ec6ea90d01f8170e11ed97d715ea82
azunisoftware18 <azunisoftware18@gmail.com>
2025-08-28 13:53:35 +0530

    `Added logging statements for Razorpay key ID and secret`

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,12 +9,12 @@ import crypto from "crypto";
 
 const MAX_INT = Number.MAX_SAFE_INTEGER;
 
+console.log("Razorpay ID:", process.env.RAZORPAY_KEY_ID);
+console.log("Razorpay ID:", process.env.RAZORPAY_KEY_SECRET);
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
-console.log("Razorpay ID:", process.env.RAZORPAY_KEY_ID);
-console.log("Razorpay ID:", process.env.RAZORPAY_KEY_SECRET);
 
 // ======================= PLAN CONFIG ======================
 const planPricesUSD = {
